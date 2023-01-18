47216c94e9fced1fc121aad271022895a2db60f2
Eshant Manghnani <eshant.manghnani@softvision.com>
2023-01-18 17:40:29 +0800

    Change theme default export

--- pages/context/themes.js
+++ pages/context/themes.js
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 
 export const Context = createContext([[],() => {}])
 
-export function ThemeProvider({ children }) {
+export default function ThemeProvider({ children }) {
     const [darkMode, setDarkMode] = useState(!darkMode);
     return (
       <Context.Provider value={[theme, setTheme]}>{children}</Context.Provider>
